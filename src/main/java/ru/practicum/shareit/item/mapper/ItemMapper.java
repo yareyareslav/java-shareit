2b6e7bfeb022a5c9824fa6b0dd4ba0544fd3c32b
Makanin Yaroslav <fobosguychanel@gmail.com>
2026-05-24 16:09:45 +0300

    chore: unused imports

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.item.mapper;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.Item;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.request.ItemRequest;
-import ru.practicum.shareit.user.User;
 
 @Component
 public class ItemMapper {
