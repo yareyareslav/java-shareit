1121c2d8604a32704fdfecefa19f3c0297275e6a
Makanin Yaroslav <fobosguychanel@gmail.com>
2026-05-29 21:47:30 +0300

    fix: props

--- src/test/java/ru/practicum/shareit/item/ItemServiceTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.practicum.shareit.booking.BookingConstantsTest;
 import ru.practicum.shareit.booking.BookingRepository;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ResponseItemDto;
